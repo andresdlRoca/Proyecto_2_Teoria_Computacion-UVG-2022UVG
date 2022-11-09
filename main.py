eb30cb8608ebf8a26703921457a3561724701ca6
andresdlRoca <andresdelaroca11@gmail.com>
2022-11-09 11:33:19 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+######################################
+#
+# Teoria de la computacion
+# Grupo 9:
+# Yong Park
+# Jun Woo Lee
+# Andres de la Roca
+#
+# Proyecto 2
+#
+######################################
+
+# Gramaticas
+
+
+
+# Funciones Lambda
+
+
